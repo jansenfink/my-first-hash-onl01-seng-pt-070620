975849d14fe4c53f123ad6ce35b93ee2f9a45ffa
Jansen Fink <jansenfink@charter.net>
2020-07-18 13:04:45 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -36,6 +36,9 @@ def adding
                  }
                  
       shipping_manifest["muskets"] = 2 
+      shipping_manifest["gun powder"] = 4
+      
+      return shipping_manifest
       
   # add 2 muskets to the shipping_manifest hash below
 
